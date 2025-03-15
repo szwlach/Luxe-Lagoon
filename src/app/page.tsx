c672b37d62e50dfcd2f2f476981b326769a1d07b
vendetta106 <vendetta10668@gmail.com>
2025-03-16 00:25:43 +0100

    margin top on small devices on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import ButtonMain from "./components/ButtonMain";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center min-h-screen w-full overflow-x-hidden">
+    <div className="flex flex-col items-center min-h-screen w-full overflow-x-hidden mt-24">
       <Navbar />
       <div className="w-full px-2 sm:px-4 max-w-screen-xl mx-auto">
         <h1 className="text-white font-sans font-semibold w-full">
           <TypingSmooth />
         </h1>
       </div>
-      <div className="mt-8 sm:mt-6 md:mt-8">
+      <div className="mt-12 sm:mt-6 md:mt-8">
         <ButtonMain />
       </div>
     </div>
